a342a85f936ac37b30f9f8e80925d6d5dedfe128
zlatnaspirala <zlatnaspirala@gmail.com>
2020-11-23 22:08:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -36,9 +36,30 @@ var options = null;
 app.use(cors());
 
 // express.compress()
-app.use(compression({
+/*app.use(compression({
   filter: function () { return true; }
 }));
+*/
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use(
   bodyParser.urlencoded({
@@ -93,7 +114,7 @@ app.get("/rocket/user", (req, res) => {
  * @description
  * Almost any case
  */
-app.post("/rocket/register", cors(), (req, res) => {
+app.post("/rocket/register", (req, res) => {
 
   console.log("POST DETECTED");
   if (req.secure) {
