ef88b8b85253e1d219ac627f221294d0280b92cc
zlatnaspirala <zlatnaspirala@gmail.com>
2020-11-27 23:10:17 +0100

    [wip]

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
-class ServerConfig {
-  constructor() {
-    /**
-     * Define backend staff here
-     */
+/**
+ * @description Define backend staff here.
+ * @param serverMode { `dev` or `prod` }
+ * @param networkDeepLogs { Boolean }
+ * @param connectorPort { number }
+ */
 
+class ServerConfig {
+
+  constructor() {
+  
     // enum : 'dev' or 'prod'
     this.serverMode = "dev";
 
@@ -32,32 +37,39 @@ class ServerConfig {
       pCBPath: "/etc/httpd/conf/ssl/maximumroulette.ca-bundle"
     };
 
-    this.appUseAccountsSystem = true;
+    /**
+     * @description Database name
+     * @param databaseName
+     * @param databaseRoot {
+     *   dev => LocalHost usage
+     *   prod => Public Domain with possible remote connection
+     *   secured => Public Domain with no possible remote connection
+     * }
+     */
     this.databaseName = "rocket-master-base1";
 
     this.databaseRoot = {
       dev: "mongodb://localhost:27017",
-      prod: "mongodb://userAdmin:********@localhost:27017/admin"
+      prod: "mongodb://userAdmin:********@maximumroulette.com:27017/admin",
+      secured: "mongodb://userAdmin:********localhost:27017/admin",
     };
 
     this.specialRoute = {
       default: "/var/www/html/applications/"
     };
 
-    console.log(" <--------------------------------------------");
-    console.log(" -> Server running under " + this.serverMode + " configuration.");
+    console.info(" --------------------------------------------");
+    console.info(" -> Server running under " + this.serverMode + " configuration.");
     if (this.serverMode == "dev") {
-      console.log(" -> domain dev:", this.domain.dev);
+      console.info(" -> domain dev:", this.domain.dev);
     } else if (this.serverMode == "prod") {
-      console.log(" -> domain prod:", this.domain.prod);
+      console.info(" -> domain prod:", this.domain.prod);
     }
-
-    console.log(" -> masterServerKey:", this.masterServerKey);
-    console.log(" -> connectorPort:", this.connectorPort);
-    console.log(" -> protocol:", this.protocol);
-    console.log(" -> appUseAccountsSystem:", this.appUseAccountsSystem);
-    console.log(" -> databaseName:", this.databaseName);
-    console.log(" <--------------------------------------------");
+    console.info(" -> masterServerKey:", this.masterServerKey);
+    console.info(" -> connectorPort:", this.connectorPort);
+    console.info(" -> protocol:", this.protocol);
+    console.info(" -> databaseName:", this.databaseName);
+    console.info(" --------------------------------------------");
   }
 
   get getDatabaseRoot() {
@@ -66,10 +78,6 @@ class ServerConfig {
     } else if (this.serverMode == "prod") {
       return this.databaseRoot.prod;
     }
-  }
-
-  get IsDatabaseActive() {
-    return this.appUseAccountsSystem;
   }
 
   set setNetworkDeepLog(newState) {
