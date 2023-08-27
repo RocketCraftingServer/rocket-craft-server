f55563441bddd47e178e36e131b480dc83066f08
zlatnaspirala <zlatnaspirala@gmail.com>
2023-08-27 21:43:08 +0200

    Update mail-service.js

--- email/mail-service.js
+++ email/mail-service.js
@@ -1,9 +1,5 @@
-/**
- * This class use shared lib.
- */
-const shared = require("./../common/shared");
-const config = require("./../config");
-
+const shared = require("../common/shared");
+const nodemailer = require('nodemailer')
 /**
  * @description Only objective for this class is
  * sending emails.
@@ -11,36 +7,49 @@ const config = require("./../config");
 class Sender {
 
   constructor(to, subject, content) {
-
     this.userId = shared.formatUserKeyLiteral(to);
     this.subject = subject;
     this.to = to;
     this.content = content;
-
   }
 
   async SEND(subject) {
-
-    let arg = {
-      user: "greespiral@gmail.com",
-      pass: "******",
-      to: this.to,
-      subject: this.subject,
-      html: this.content
-    }
-
-    return new Promise((resolve, reject) => {
-      this.GM = require("gmail-send")(arg);
-      this.GM((
-        subject
-        // files: [filepath],
-      ), function(err, res) {
-        if (err != null) { reject(err); }
-        resolve(res);
-      });
+    const transporter = nodemailer.createTransport({
+      host: "live.smtp.mailtrap.io",
+      port: 587,
+      tls: {
+        secure: false,
+        ignoreTLS: true,
+        rejectUnauthorized: false
+      },
+      auth: {
+        user: "api",
+        pass: "*********",
+      }
     });
 
-  }
+    transporter.verify(function(error, success) {
+      if(error) {
+        console.log("EMAIL SERVICE FAILED" + error);
+      } else {
+        console.log('Server validation done and ready for messages.')
+      }
+    });
 
+    const email = {
+      from: 'mailtrap@maximumroulette.com',
+      to: this.to,
+      subject: this.subject,
+      text: this.content
+    };
+
+    transporter.sendMail(email, function(error, success) {
+      if(error) {
+        console.log('Nodemailer Email failed: ' + error);
+      } else {
+        console.log('Nodemailer Email sent: ' + success.response);
+      }
+    })
+  }
 }
-module.exports = (to, subject, content) => { return new Sender(to, subject, content) }
+module.exports = (to, subject, content) => {return new Sender(to, subject, content)}
